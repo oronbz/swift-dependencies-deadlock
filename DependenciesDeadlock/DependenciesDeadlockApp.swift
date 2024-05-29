7bd3071de607e7a5cab68fb746df9de9d63b9e3f
Oron Ben Zvi <oronbz@gmail.com>
2024-05-29 18:42:25 +0300

    Initial Commit

---
+++ DependenciesDeadlock/DependenciesDeadlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependenciesDeadlockApp.swift
+//  DependenciesDeadlock
+//
+//  Created by Oron Ben Zvi on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DependenciesDeadlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
